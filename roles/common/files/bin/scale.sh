ec811c6dee7c65773fcb8244af73ef423006b650
Benoit Delbosc <bdelbosc@nuxeo.com>
2023-06-07 12:46:50 +0200

    fix scale.sh

--- roles/common/files/bin/scale.sh
+++ roles/common/files/bin/scale.sh
@@ -4,5 +4,5 @@ SERVER_URL=${SERVER_URL:-http://nuxeo.docker.localhost}
 set -e
 set -x
 SCRIPT_PATH="$(cd "$(dirname "$0")"; pwd -P)"
-curl -X DELETE -u Administrator:Administrator "${SERVER_URL}/nuxeo/api/v1/management/stream/scale" | jq .
+curl -X GET -u Administrator:Administrator "${SERVER_URL}/nuxeo/api/v1/management/stream/scale" | jq .
 
